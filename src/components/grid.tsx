2c94289f56d58cb6ed26e91ef8ba4c52a664e4f3
LightKazuto <ilhamsidi62@gmail.com>
2024-08-08 05:55:48 +0700

    feat: add url for image testing

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -14,23 +14,23 @@ export default function Home() {
   const [products, setProducts] = useState<Product[]>([
     {
       id: 1,
-      image: '/path/to/image1.jpg',
+      image: 'https://th.bing.com/th/id/OIP._ysCJqtfVyIKHdE-yECJzgHaE8?rs=1&pid=ImgDetMain',
       title: 'Product 1',
       price: 'Rp. 100.000',
       description: '4.5'
     },
     {
       id: 2,
-      image: '/path/to/image2.jpg',
+      image: 'https://th.bing.com/th/id/R.936f2fc892e5132164c645ac7b6ea719?rik=LrXhfKREcwcTtA&riu=http%3a%2f%2flugualami.net%2fwp-content%2fuploads%2f2017%2f01%2fLugu-Alami-Khasiat-Kangkung-1.jpg&ehk=f6Teh5ZvZKXtemSeFWysf0eS41Rru0jEym0CeE%2bTmJE%3d&risl=&pid=ImgRaw&r=0',
       title: 'Product 2',
       price: 'Rp.99.000',
       description: '4.2'
     },
     {
       id: 3,
-      image: '/path/to/image3.jpg',
+      image: 'https://th.bing.com/th/id/OIP.fkydlyArZwW4BfFYe3rVFgHaFg?rs=1&pid=ImgDetMain',
       title: 'Product 3',
-      price: '$39.99',
+      price: 'Rp.10.000',
       description: '4.8'
     }
   ]);
